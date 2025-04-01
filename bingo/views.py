ee0608cc37796a9742b48ad83628bbe64df8730e
Jere283 <jeremyfigueroa2015@outlook.com>
2025-04-01 01:39:19 -0600

    datetime fix

--- bingo/views.py
+++ bingo/views.py
@@ -1,4 +1,5 @@
 from django.db import IntegrityError
+from django.utils.timezone import make_aware
 from rest_framework import viewsets, filters, status
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import GenericAPIView
@@ -52,8 +53,11 @@ class MarcarComoEntregado(GenericAPIView):
 
             usuario = User.objects.get(username=request.user.username)
 
+            gmt_6 = pytz.timezone("America/Guatemala")  
+            fecha_entrega_gmt6 = make_aware(datetime.now(), gmt_6)
+
             participante.entregado = True
-            participante.fecha_entrega = datetime.now()
+            participante.fecha_entrega = fecha_entrega_gmt6
             participante.entregado_por = usuario
             participante.save()
             serializer = self.serializer_class(instance=participante)
